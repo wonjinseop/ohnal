0ea6e31138c583d936ec56aacdb81f9351eab2dc
goodperiodt <ilypsj@gmail.com>
2024-04-11 18:36:15 +0900

    first commit - ohnal project

---
+++ src/main/java/com/ohnal/OhnalApplication.java
@@ -0,0 +1,13 @@
+package com.ohnal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhnalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhnalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohnal/OhnalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohnal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhnalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
