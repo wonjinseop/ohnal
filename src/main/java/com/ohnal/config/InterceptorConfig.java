c6330379fa6ee1846d968bd229b344ba247070de
goodperiodt <ilypsj@gmail.com>
2024-04-19 17:45:16 +0900

    interceptor 설정 완료

--- src/main/java/com/ohnal/config/InterceptorConfig.java
+++ src/main/java/com/ohnal/config/InterceptorConfig.java
@@ -1,10 +1,26 @@
 package com.ohnal.config;
 
-// 내가 만든 인터셉터들을 스프링 컨텍스트에 등록하는
-public class InterceptorConfig {
+import com.ohnal.interceptor.AfterLoginInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 내가 만든 인터셉터들을 스프링 컨텍스트에 등록하는 설정 파일
+@Configuration
+@RequiredArgsConstructor
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    private final AfterLoginInterceptor afterLoginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 로그인 후 비회원 전용 페이지 접근 차단 인터셉터 등록
+        registry
+                .addInterceptor(afterLoginInterceptor) // 어떤 인터셉터를 등록할 것인지
+                .addPathPatterns("/members/sign-up", "/members/sign-in"); // 어떤 요청에서 인터셉터를 동작하게 할 것인지
 
 
 
-
-
+    }
 }
--- src/main/java/com/ohnal/interceptor/AfterLoginInterceptor.java
+++ src/main/java/com/ohnal/interceptor/AfterLoginInterceptor.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.io.PrintWriter;
+
 @Configuration
 public class AfterLoginInterceptor implements HandlerInterceptor {
     // 로그인 한 이후 회원만 접근할 수 있는 페이지
@@ -18,13 +20,34 @@ public class AfterLoginInterceptor implements HandlerInterceptor {
 
         // 세션 데이터 중 login이라는 이름의 데이터가 있는지 확인한 후에
         // 만약 존재한다면 요청이 컨트롤러로 들어가지 못하게 하겠다.
-        if(session.getAttribute("login") != null) {
-            response.sendRedirect("/index");
+        if (session.getAttribute("login") != null) {
+
+            response.setContentType("text/html; charset=UTF-8");
+
+            PrintWriter w = response.getWriter();
+            String htmlCode = "<!DOCTYPE html>\n" +
+                    "<html lang=\"ko\">\n" +
+                    "<head>\n" +
+                    "  <meta charset=\"UTF-8\">\n" +
+                    "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                    "  <title>Document</title>\n" +
+                    "</head>\n" +
+                    "<body>\n" +
+                    "\n" +
+                    "  <script>\n" +
+                    "    alert('이미 로그인 한 회원입니다.');\n" +
+                    "    location.href='/index';\n" +
+                    "  </script>\n" +
+                    "  \n" +
+                    "</body>\n" +
+                    "</html>";
+            w.write(htmlCode);
+            w.flush();
+
             return false; // 컨트롤러로 들어가는 요청을 막음
         }
-
         return true; // 로그인 안했으면 컨트롤러로 요청이 들어간다.
-
-
     }
+
+
 }
